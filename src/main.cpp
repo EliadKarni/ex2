eb70c4c893c049f6c89c2f1b87fb5e3168aa769c
Eliad Karni <EliadK995@Gmail.com>
2020-11-15 13:12:22 +0200

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+﻿/* ex2
+ * ===========================================================================
+ * Writen by:
+ * Sharon Levi | ID: 311593313 | login: sharonlevi
+ * Eliad Karni | ID: 206329104 | login: eliadka
+ *
+ *
+ */
+
+ //---------------------------- include section -------------------------------
+#include <iostream>
+//------------------------------ using section -------------------------------
+using std::cin;
+using std::cout;
+using std::endl;
+//------------------------------ main section --------------------------------
+int main()
+{
+	cout << "tnx Mich." << endl;
+	return 0;
+}
