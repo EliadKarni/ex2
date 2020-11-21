2667589faf4384c6fea36cf87ac8b1474af9a7b8
Eliad Karni <EliadK995@Gmail.com>
2020-11-21 16:32:26 +0200

    מחיקות

--- include/Ladder.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Location.h"
-class Ladder {
-private:
-	Location Begin, End;
-public:
-	Ladder(const Location& begin,const Location& end);
-};
--- include/Rod.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Location.h"
-class Rod
-{
-public:
-	Rod(const Location& begin, const Location& end);
-private:
-	Location Begin, End;
-};
