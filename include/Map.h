e24d378cc5bcff473db615bb3665bd833f7e22ea
Sharon Levi <73107447+sharonlevi94@users.noreply.github.com>
2020-11-22 22:16:52 +0200

    Update Map.h

--- include/Map.h
+++ include/Map.h
@@ -1,26 +1,42 @@
 #pragma once
-
+#include <fstream>
+#include <iostream>
+#include"Location.h"
+#include "Coin.h"
+#include "LinkedList.h"
+#include <ostream>
+#include "io.h"
 #include <vector>
-#include "Location.h"
-
+using std::ifstream;
 using std::vector;
-
+//===============================================
+#define NOTHING ' '
 #define WALL '#'
+#define FLOOR '#'
 #define LADDER 'H'
 #define ROD  '-'
+#define ON_LADDER 'S'
 
-#define CLIME 'c'
-#define WALK 'w'
-#define FALL 'f'
-
+#define PLAYER '@'
+#define ENEMY '%'
+//===============================================
 class Map {
 private:
-	vector<vector<char>> *stageMap;
-	int MapSize;
+	vector<vector<char>> StageMap;
+	int MapSize, NumOfCoins; 
+	Location InitialPlayerLocation;
+	vector<Location> InitalsEnemyLocations;
 	
 public:
-	Map(vector<vector<char>> *input = nullptr) : stageMap(input) {}
-	~Map();
-	Location isMovePossible(const Location& precentLoc, int wantedMove);
-	Location calcEnemyMove(const Location& precentLoc, const Location& playerLoc);//add find shortest path method
+	Map(ifstream&);
+	bool isCoinExist(const Location& point);
+	vector<vector<char>> getStageMap()const;
+	int getMapSize() const;
+	void CreateStageMap(ifstream&);
+	Location isMovePossible(const Location&, int);
+	Location GetLocationAfterFallDown(Location);
+	Location UpMove(Location);
+	Location DownMove(Location);
+	Location RightMove(Location);
+	Location LeftMove(Location);
 };
