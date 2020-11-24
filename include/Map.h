68f29f0f5f0a07f109b87cc111ac59ee8857ab8f
Sharon Levi <73107447+sharonlevi94@users.noreply.github.com>
2020-11-24 16:27:27 +0200

    Update Map.h

--- include/Map.h
+++ include/Map.h
@@ -29,17 +29,18 @@ public:
 	Map(ifstream&);
 	bool isCoinExist(const Location& point);
 	
-	vector<vector<char>> getStageMap()const;
-	int getMapSize() const;
-	Location GetInitialPlayerLocation() const;
-	vector<Location> GetInitalsEnemyLocationsList() const;
+	vector<vector<char>> getStageMap()               const;
+	int getMapSize()                                 const;
+	Location GetInitialPlayerLocation()              const;
+	vector<Location> GetInitalsEnemyLocationsList()  const;
 	
-	void CreateStageMap(ifstream&);
-	Location isMovePossible(const Location&, int);
-	Location GetLocationAfterFallDown(Location);
-	Location UpMove(Location);
-	Location DownMove(Location);
-	Location RightMove(Location);
-	Location LeftMove(Location);
-	Location EnemyMove(Location);
+	void CreateStageMap                        (ifstream&);
+	Location isMovePossible         (const Location&, int);
+	Location GetLocationAfterFallDown    (const Location&);
+	Location UpMove                      (const Location&);
+	Location DownMove                    (const Location&);
+	Location RightMove                   (const Location&);
+	Location LeftMove                    (const Location&);
+	Location FoolEnemy                   (const Location&);
+	Location SmartEnemy(const Location&, const Location& );
 };
