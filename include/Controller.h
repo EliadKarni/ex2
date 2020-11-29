e025f29ce83921bebf6db95787f20bad78a246cb
Sharon Levi <73107447+sharonlevi94@users.noreply.github.com>
2020-11-29 09:39:34 +0200

    Update Controller.h

--- include/Controller.h
+++ include/Controller.h
@@ -1,3 +1,6 @@
+#ifndef _Controller_H
+#define _Controller_H
+
 #pragma once
 //---------------------------- include section -------------------------------
 #include <string.h>
@@ -37,3 +40,4 @@ private:
 	PlayerState m_state;
 	BoardReader m_boardReader;
 };
+#endif // !__Controller_H
