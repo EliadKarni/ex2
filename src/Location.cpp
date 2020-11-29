14ddbf5141f673e2b7a94c268dbb2369b898927c
Sharon Levi <73107447+sharonlevi94@users.noreply.github.com>
2020-11-29 19:14:27 +0200

    Update Location.cpp

--- src/Location.cpp
+++ src/Location.cpp
@@ -1,6 +1,6 @@
 #include "Location.h"
 
-/*----------------------------------------------------------------------------
+//========================================================================
  * The method compare the between the locations and returns if their equals.
  * input: Location wonted to be compared to.
  * output: if the locations equals.
@@ -8,7 +8,7 @@
 bool Location::operator==(const Location& location)const {
 	return(this->col == location.col && this->row == location.row);
 };
-/*----------------------------------------------------------------------------
+//========================================================================
  * The method 
  * input: .
  * output: .
@@ -19,18 +19,24 @@ bool inRectangle(const Location& topLeft, const Location& botRight,
 		blow = isBelowOf(location, botRight),
 		left = isLeftOf(location, topLeft),
 		right = isRightOf(location, botRight);
-	return(!(isAboveOf(location, topLeft) || isLeftOf(location, topLeft) ||
-		isBelowOf(location, botRight) || isRightOf(location, botRight)));
+	return(!(isAboveOf(location, topLeft) ||
+		 isLeftOf(location, topLeft) ||
+		isBelowOf(location, botRight) || 
+		 isRightOf(location, botRight)));
 }
+//========================================================================
 bool isLeftOf(const Location& testLoc, const Location& loc) {
 	return(loc.col > testLoc.col);
 }
+//========================================================================
 bool isRightOf(const Location& testLoc, const Location& loc) {
 	return(!isLeftOf(testLoc,loc));
 }
+//========================================================================
 bool isAboveOf(const Location& testLoc, const Location& loc) {
 	return(loc.row > testLoc.row);
 }
+//========================================================================
 bool isBelowOf(const Location& testLoc, const Location& loc) {
 	return(!isAboveOf(testLoc, loc));
 }
